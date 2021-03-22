fea4d8ceabaa7ccf076319ac167bcd56dd4be0c9
ShKlinkenberg <s.klinkenberg@gmail.com>
2021-03-22 16:40:52 +0100

    deleted blank line

--- CompileItems.R
+++ CompileItems.R
@@ -2,9 +2,8 @@
 
 # install.packages("exams", dependencies = TRUE)
 library(exams)
-ItemRmdPath = "example/uva-test-en-001.Rmd" # Select the path to your file
-DirPath     = substr(ItemRmdPath, 1, rev(gregexpr('/',ItemRmdPath)[[1]])[1] )
+ItemRmdPath = "example/test1/uva-test-en-001.Rmd" # Select the path to your file
+DirPath     = substr(ItemRmdPath, 1, sapply(gregexpr("/", ItemRmdPath), tail, 1) )
 saveHTML    = paste0(substring(ItemRmdPath, nchar(DirPath)+1, nchar(ItemRmdPath)-4))
 
 exams2html(file = ItemRmdPath, dir = DirPath, name = saveHTML)
-
