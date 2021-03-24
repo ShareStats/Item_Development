36e12f6e5bd99e786298901385e18876f9168cab
ShKlinkenberg <s.klinkenberg@gmail.com>
2021-03-24 17:40:05 +0100

    Templates Created

---
+++ ShareStatsItemsProject/CompileIdtems.R
@@ -0,0 +1,8 @@
+# Compile items
+
+# install.packages("exams", dependencies = TRUE)
+library("exams")
+
+# Keep a record of the items you have compiled in this project
+exams2html(file = "exampleProject/Descriptive_statistics/uva-test-en-001/uva-test-en-001.Rmd")
+exams2html(file = "")
--- exampleProject/CompileIdtems.R
+++
@@ -1,7 +0,0 @@
-# Compile items
-
-# install.packages("exams", dependencies = TRUE)
-library(exams)
-
-# Keep a record of the items you have compiled in this project
-exams2html(file = "")
